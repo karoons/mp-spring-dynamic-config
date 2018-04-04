972e1015a5dd241ae6a43aa46e8b7cd71e5913f4
karoon sillapapan <k.sillapapam@gmail.com>
2018-04-04 09:42:50 +0700

    try to config rest webservice

--- src/main/java/com/service/mp/spring/dynamic/config/Application.java
+++ src/main/java/com/service/mp/spring/dynamic/config/Application.java
@@ -7,16 +7,23 @@ package com.service.mp.spring.dynamic.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
  * @author Karoons
  */
 @SpringBootApplication
-@Controller
 public class Application {
-        public static void main(String[] args) throws Exception {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
